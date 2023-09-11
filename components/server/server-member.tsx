11dc5b5ae16213b5bcc676cbd585865acd3a1c77
benmhara <benmharamalek@gmail.com>
2023-09-11 04:13:28 +0100

    Edit Channels

--- components/server/server-member.tsx
+++ components/server/server-member.tsx
@@ -26,9 +26,14 @@ export const ServerMember = ({ member, server }: ServerMemberProps) => {
 
   const icon = roleIconMap[member.role]
 
+  const onClick = () => {
+    router.push(`/servers/${params?.serverId}/conversations/${member.id}`)
+  }
+
   return (
     <div>
       <button
+        onClick={onClick}
         className={cn(
           'group px-2 py-2 rounded-md flex items-center gap-x-2 w-full hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition mb-1',
           params?.memberId === member.id && 'bg-zinc-700/20 dark:bg-zinc-700'
@@ -41,10 +46,12 @@ export const ServerMember = ({ member, server }: ServerMemberProps) => {
         <p
           className={cn(
             'font-semibold text-sm text-zinc-500 group-hover:text-zinc-600 dark:text-zinc-400 dark:group-hover:text-zinc-300 transition',
-            params?.channelId === member.id &&
+            params?.memberId === member.id &&
               'text-primary dark:text-zinc-200 dark:group-hover:text-white'
           )}
-        ></p>
+        >
+          {member.profile.name}
+        </p>
         {icon}
       </button>
     </div>
